32b6b8844d001fffa8d60bf53056e4b8ce345af1
Sorbete-Sempai123 <tipon_tn@hotmail.com>
2018-10-11 13:23:12 -0500

    Version 2.0

--- pruebasx2.cpp
+++ pruebasx2.cpp
@@ -1,15 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-int main()
+ int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 450), "SFML works!");
-    sf::CircleShape shape(10.f);
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+    sf::RectangleShape shape(sf::Vector2f(20,20));
     shape.setFillColor(sf::Color::Blue);
     sf::CircleShape ward(2.f);
     ward.setFillColor(sf::Color::Red);
-
-    while (window.isOpen()){
+    int acum=0,aux=1;
+     while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
@@ -18,23 +17,30 @@ int main()
         window.clear();
         window.draw(shape);
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-        	window.draw(ward);
+        	ward.setPosition(shape.getPosition().x,shape.getPosition().y);
+            acum=3000;
+            window.draw(ward);
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)&& shape.getPosition().x<=620){
         	shape.move(sf::Vector2f(0.1,0));
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)&& shape.getPosition().x>=0){
         	shape.move(sf::Vector2f(-0.1,0));
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && shape.getPosition().y>=0){
         	shape.move(sf::Vector2f(0,-0.1));
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && shape.getPosition().y<=400){
-        	shape.move(sf::Vector2f(0,0.100));
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && shape.getPosition().y<=460){
+        	shape.move(sf::Vector2f(0,0.1));
         }
-        std::cout<<shape.getPosition().y<<std::endl;
+        if(shape.getPosition().y<=460 || acum>=0){
+            if(acum-->0)
+                shape.move(sf::Vector2f(0,-0.05));
+            else
+                shape.move(sf::Vector2f(0,0.05));
+        }
+        std::cout<<shape.getPosition().y<<" :: "<<shape.getPosition().x<<" :: "<<acum<<std::endl;
         window.display();
     }
-
-    return 0;
-}
+     return 0;
+} 
