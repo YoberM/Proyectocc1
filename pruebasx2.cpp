7d101deb3223f2b06b253692cab6a70ad2935ae3
cs-unsa11 <tipon_tn@hotmail.com>
2018-10-29 13:31:57 -0500

    Prueba3

--- pruebasx2.cpp
+++ pruebasx2.cpp
@@ -1,13 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
- int main()
+#include "Enemy.h"
+#define ventanax 640
+#define ventanay 640
+#define tamper 40 
+int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
-    sf::RectangleShape shape(sf::Vector2f(20,20));
-    shape.setFillColor(sf::Color::Blue);
+    sf::RenderWindow window(sf::VideoMode(ventanax, ventanay), "SFML works!");
+    sf::RectangleShape shape(sf::Vector2f(tamper,tamper));
     sf::CircleShape ward(2.f);
     ward.setFillColor(sf::Color::Red);
-    int acum=0,aux=1;
+        sf::Texture texturas;
+        texturas.loadFromFile("pokeball.png");
+        shape.setTexture(&texturas,1);
+        int acum=0;
+    char dir='W';
      while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
@@ -16,30 +23,24 @@
         }
         window.clear();
         window.draw(shape);
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-        	ward.setPosition(shape.getPosition().x,shape.getPosition().y);
-            acum=3000;
-            window.draw(ward);
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)&& shape.getPosition().x<=620){
-        	shape.move(sf::Vector2f(0.1,0));
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)&& shape.getPosition().x<=ventanax-tamper){
+            shape.move(sf::Vector2f(0.1,0));
+            dir='D';
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)&& shape.getPosition().x>=0){
-        	shape.move(sf::Vector2f(-0.1,0));
+            shape.move(sf::Vector2f(-0.1,0));
+            dir='A';
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && shape.getPosition().y>=0){
-        	shape.move(sf::Vector2f(0,-0.1));
+            shape.move(sf::Vector2f(0,-0.1));
+            dir='W';
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && shape.getPosition().y<=460){
-        	shape.move(sf::Vector2f(0,0.1));
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && shape.getPosition().y<=ventanay-tamper){
+            shape.move(sf::Vector2f(0,0.1));
+            dir='S';
         }
-        if(shape.getPosition().y<=460 || acum>=0){
-            if(acum-->0)
-                shape.move(sf::Vector2f(0,-0.05));
-            else
-                shape.move(sf::Vector2f(0,0.05));
-        }
-        std::cout<<shape.getPosition().y<<" :: "<<shape.getPosition().x<<" :: "<<acum<<std::endl;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::E)) return 0;
+        std::cout<<shape.getPosition().y<<" :: "<<shape.getPosition().x<<" :: "<<dir<<std::endl;
         window.display();
     }
      return 0;
